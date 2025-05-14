e0417fe7372d60bb1f841456fba096d134a3afe6
amira belgharat <amira.belgharat.21@gmail.com>
2025-05-14 15:35:19 -0400

    création de la validation du formulaire + modifications HTML et CSS
    - Ajout d’un script JavaScript pour valider les champs email et message
    - Affichage des messages d’erreur sous chaque champ en cas d’erreur
    - Ajout d’IDs uniques pour les erreurs dans le HTML
    - Création d’un fichier CSS externe avec stylisation du formulaire
    - Amélioration de la mise en page (labels, boutons, champs, etc.)

---
+++ script/validation.js
@@ -0,0 +1,123 @@
+
+const formulaire = document.getElementById("contactForm");
+const nom = document.getElementById("nom");
+const prenom = document.getElementById("prenom");
+const email = document.getElementById("email");
+const msg = document.getElementById("message");
+
+
+
+
+  formulaire.addEventListener("submit", (e) => {
+ 
+    e.preventDefault();
+    if (validateInputs()) { 
+      formulaire.submit(); 
+    
+
+  }
+
+  });
+    const   validateInputs =() =>
+
+
+
+
+      {
+          let noError =true;
+          const nomValue = nom.value.trim();
+          const prenomValue = prenom.value.trim();
+          const emailValue = email.value.trim(); 
+          const messageValue = msg.value.trim(); 
+        
+      
+          if (nomValue === "") {
+              setError(nom, 'nom  est requis ');
+              noError=false;
+          } 
+      
+          else if (isNameTooLong(nomValue)) {
+              setError(nom, 'Le nom est trop long !');
+              noError = false;
+          }
+          else {
+              setSuccess(nom);
+          }
+          if (prenomValue === "") {
+            setError(prenom, 'prenom est requis ');
+            noError=false;
+        } 
+    
+        else if (isLastnameTooLong(prenomValue)) {
+            setError(prenom, 'Le prenom est trop long !');
+            noError = false;
+        }
+        else {
+            setSuccess(prenom);
+        }
+            if (emailValue === "") {
+              setError(email, " L'email est requis  !");
+              noError = false;
+            }
+            else if (!isValidEmail(emailValue)) {
+              setError(email, 'Adresse email incorrecte. Veuillez réessayer');
+          }
+            else {
+              setSuccess(email);
+      
+          }
+            if (messageValue === "") {
+              setError(msg, "message est requis  !");
+              noError=false;
+            }
+      
+            else if (isMessageTooLong(messageValue)) {
+              setError(msg, 'Message est trop long !');
+              noError = false;
+          }
+          else {
+              setSuccess(msg);
+          }
+
+      }
+      
+      
+          const setError = (element, message) => {
+              const inputControl = element.parentElement;
+              const errorDisplay = inputControl.querySelector('.errormessage');
+          
+              errorDisplay.innerText = message;
+              inputControl.classList.add('error');
+              inputControl.classList.remove('success');
+          }
+      
+          const setSuccess = element => {
+              const inputControl = element.parentElement;
+              const errorDisplay = inputControl.querySelector('.errormessage');
+          
+              errorDisplay.innerText = '';
+              inputControl.classList.add('success');
+              inputControl.classList.remove('error');
+          };
+
+
+  
+
+
+const isValidEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+const isNameTooLong = nom => {
+
+  return nom.length >= 100;
+};
+const isLastnameTooLong = prenom => {
+
+  return prenom.length >= 100;
+};
+const isMessageTooLong = message => {
+
+  return message.length >= 1000;
+};
